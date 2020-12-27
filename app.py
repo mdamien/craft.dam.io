7c4e32ae33981994ac7195cfd7ee2cfaa37ff8aa
Damien <damien@dam.io>
2020-12-27 20:21:44 +0100

    load map from server

---
+++ app.py
@@ -0,0 +1,11 @@
+import json
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+@cross_origin()
+def get_map():
+    return jsonify(json.load(open('map.json')))
