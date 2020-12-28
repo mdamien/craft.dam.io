d45e0f110877f08f1f9c536abea76e9d538b2e2d
Damien <damien@dam.io>
2020-12-28 08:05:53 +0100

    store map on server

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @cross_origin()
 def get_map():
+    if request.method == 'POST':
+        map = json.loads(request.form['data'])
+        open('map.json', 'w').write(json.dumps(map))
     return jsonify(json.load(open('map.json')))
